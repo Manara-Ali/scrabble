9cdd02bbb4b83eeda0f20e2e6f4c47942c25404f
Manara-Ali <manaraali64@gmail.com>
2023-02-09 15:23:15 -0500

    Add code for modal window

--- js/app.js
+++ js/app.js
@@ -15,13 +15,64 @@
 
 ///////////////////////////////// SELECT ELEMENTS
 const body = document.querySelector("body");
+const overlay = document.querySelector(".overlay");
+// const modalElement = document.querySelector(".modal");
+const form = document.querySelector("#player-names-form");
+const player1NameInput = document.querySelector("#player-1-name");
+const player2NameInput = document.querySelector("#player-2-name");
+const playersSubmitBtn = document.querySelector(".btn-players-submit");
+const player1 = document.querySelector("#player-1");
+const player2 = document.querySelector("#player-2");
+const requiredField = document.querySelector(".required");
 
-// Blur the body after document content has loaded
+////////////// ANIMATION
+
+// Animate modal window
+gsap.from(".modal", { scale: 3, duration: 1 });
+
+/////////////// EVENT LISTENERS
 document.addEventListener("DOMContentLoaded", function () {
-  body.classList.add("blur");
+  overlay.classList.remove("hide");
 });
 
-// Create all the scrabble letters
+document.addEventListener("error", function (e) {
+  if (e.detail) {
+    requiredField.classList.remove("hide");
+    e.target.classList.add("error");
+  }
+
+  window.setTimeout(() => {
+    requiredField.classList.add("hide");
+    e.target.classList.remove("error");
+  }, 2000);
+});
+
+playersSubmitBtn.addEventListener("click", function () {
+  if (player1NameInput.value && player2NameInput.value) {
+    // Change the value of the text content for player 1 and 2
+    player1.textContent = player1NameInput.value;
+    player2.textContent = player2NameInput.value;
+
+    // Clear form fields
+    player1NameInput.value = "";
+    player2NameInput.value = "";
+
+    // Remove overlay
+    overlay.classList.add("hide");
+  } else {
+    if (!player1NameInput.value) {
+      player1NameInput.dispatchEvent(
+        new CustomEvent("error", { detail: true, bubbles: true })
+      );
+    } else if (!player2NameInput.value) {
+      player2NameInput.dispatchEvent(
+        new CustomEvent("error", { detail: true, bubbles: true })
+      );
+    }
+  }
+});
+
+///////////////////  Create a class to produce all the scrabble letters
 class Tile {
   constructor(letter, value, quantity, img) {
     this.letter = letter;
@@ -31,6 +82,7 @@ class Tile {
   }
 }
 
+// Letters and quantity
 const letters = [
   { A: 1, quantity: 9 },
   { B: 3, quantity: 2 },
@@ -60,7 +112,7 @@ const letters = [
   { Z: 10, quantity: 1 },
 ];
 
-// Create tiles
+// Create individual tiles
 const tiles = letters.map((element, index) => {
   return new Tile(
     Object.keys(element)[0],
