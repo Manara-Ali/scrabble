cc52043372f454963b778a0e746b0a34e413f718
Manara-Ali <manaraali64@gmail.com>
2023-02-10 05:08:37 -0500

    Move code around to allow use of arrow functions later on

--- js/app.js
+++ js/app.js
@@ -33,10 +33,66 @@ const pouch = document.querySelector("#pouch");
 ////////////////////////// VARAIBLES
 const player1Rack = [];
 const player2Rack = [];
+const letters = [
+  { A: 1, quantity: 9 },
+  { B: 3, quantity: 2 },
+  { C: 3, quantity: 2 },
+  { D: 2, quantity: 4 },
+  { E: 1, quantity: 12 },
+  { F: 4, quantity: 2 },
+  { G: 2, quantity: 3 },
+  { H: 4, quantity: 2 },
+  { I: 1, quantity: 9 },
+  { J: 8, quantity: 1 },
+  { K: 5, quantity: 1 },
+  { L: 1, quantity: 4 },
+  { M: 3, quantity: 2 },
+  { N: 1, quantity: 6 },
+  { O: 1, quantity: 8 },
+  { P: 3, quantity: 2 },
+  { Q: 10, quantity: 1 },
+  { R: 1, quantity: 6 },
+  { S: 1, quantity: 4 },
+  { T: 1, quantity: 6 },
+  { U: 1, quantity: 4 },
+  { V: 4, quantity: 2 },
+  { W: 4, quantity: 2 },
+  { X: 8, quantity: 1 },
+  { Y: 4, quantity: 2 },
+  { Z: 10, quantity: 1 },
+];
+
+// Create a class to produce all the scrabble letters
+class Tile {
+  constructor(letter, value, quantity, img) {
+    this.letter = letter;
+    this.value = value;
+    this.quantity = quantity;
+    this.img = img;
+  }
+}
+
+// Create individual tiles
+const tiles = letters.map((element, index) => {
+  return new Tile(
+    Object.keys(element)[0],
+    Object.values(element)[0],
+    Object.values(element)[1],
+    `${Object.keys(element)[0]}.PNG`
+  );
+});
+
+// Add blank tiles
+tiles.push(new Tile("", 0, 2, "Blank.PNG"));
 
 ////////////////////////// FUNCTIONS
+
 const random = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const selectTile = function () {
+  const tile = tiles.length && random(0, tiles.length - 1);
 };
 
 ////////////// ANIMATION
@@ -113,58 +169,58 @@ pouch.addEventListener("click", function () {
   console.log(tiles[tileIndex]);
 });
 
-///////////////////  Create a class to produce all the scrabble letters
-class Tile {
-  constructor(letter, value, quantity, img) {
-    this.letter = letter;
-    this.value = value;
-    this.quantity = quantity;
-    this.img = img;
-  }
-}
+// ///////////////////  Create a class to produce all the scrabble letters
+// class Tile {
+//   constructor(letter, value, quantity, img) {
+//     this.letter = letter;
+//     this.value = value;
+//     this.quantity = quantity;
+//     this.img = img;
+//   }
+// }
 
-// Letters and quantity
-const letters = [
-  { A: 1, quantity: 9 },
-  { B: 3, quantity: 2 },
-  { C: 3, quantity: 2 },
-  { D: 2, quantity: 4 },
-  { E: 1, quantity: 12 },
-  { F: 4, quantity: 2 },
-  { G: 2, quantity: 3 },
-  { H: 4, quantity: 2 },
-  { I: 1, quantity: 9 },
-  { J: 8, quantity: 1 },
-  { K: 5, quantity: 1 },
-  { L: 1, quantity: 4 },
-  { M: 3, quantity: 2 },
-  { N: 1, quantity: 6 },
-  { O: 1, quantity: 8 },
-  { P: 3, quantity: 2 },
-  { Q: 10, quantity: 1 },
-  { R: 1, quantity: 6 },
-  { S: 1, quantity: 4 },
-  { T: 1, quantity: 6 },
-  { U: 1, quantity: 4 },
-  { V: 4, quantity: 2 },
-  { W: 4, quantity: 2 },
-  { X: 8, quantity: 1 },
-  { Y: 4, quantity: 2 },
-  { Z: 10, quantity: 1 },
-];
+// // Letters and quantity
+// const letters = [
+//   { A: 1, quantity: 9 },
+//   { B: 3, quantity: 2 },
+//   { C: 3, quantity: 2 },
+//   { D: 2, quantity: 4 },
+//   { E: 1, quantity: 12 },
+//   { F: 4, quantity: 2 },
+//   { G: 2, quantity: 3 },
+//   { H: 4, quantity: 2 },
+//   { I: 1, quantity: 9 },
+//   { J: 8, quantity: 1 },
+//   { K: 5, quantity: 1 },
+//   { L: 1, quantity: 4 },
+//   { M: 3, quantity: 2 },
+//   { N: 1, quantity: 6 },
+//   { O: 1, quantity: 8 },
+//   { P: 3, quantity: 2 },
+//   { Q: 10, quantity: 1 },
+//   { R: 1, quantity: 6 },
+//   { S: 1, quantity: 4 },
+//   { T: 1, quantity: 6 },
+//   { U: 1, quantity: 4 },
+//   { V: 4, quantity: 2 },
+//   { W: 4, quantity: 2 },
+//   { X: 8, quantity: 1 },
+//   { Y: 4, quantity: 2 },
+//   { Z: 10, quantity: 1 },
+// ];
 
-// Create individual tiles
-const tiles = letters.map((element, index) => {
-  return new Tile(
-    Object.keys(element)[0],
-    Object.values(element)[0],
-    Object.values(element)[1],
-    `${Object.keys(element)[0]}.PNG`
-  );
-});
+// // Create individual tiles
+// const tiles = letters.map((element, index) => {
+//   return new Tile(
+//     Object.keys(element)[0],
+//     Object.values(element)[0],
+//     Object.values(element)[1],
+//     `${Object.keys(element)[0]}.PNG`
+//   );
+// });
 
-// Add blank tiles
-tiles.push(new Tile("", 0, 2, "Blank.PNG"));
+// // Add blank tiles
+// tiles.push(new Tile("", 0, 2, "Blank.PNG"));
 
 // Let players pick on letter each to determine turns
 // console.log(tiles);
